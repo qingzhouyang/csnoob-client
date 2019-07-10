f47110375cd47cb47b53d1a5a69aa88188d07199
junyang <jun.yang05@sap.com>
2019-07-10 16:01:18 -0400

    modified header a bit

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -15,27 +15,25 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
     this.items = [
       {
-          label: 'File',
-          items: [{
-                  label: 'New', 
-                  icon: 'pi pi-fw pi-plus',
-                  items: [
-                      {label: 'Project'},
-                      {label: 'Other'},
-                  ]
-              },
-              {label: 'Open'},
-              {label: 'Quit'}
-          ]
+          label: 'A part'
       },
       {
-          label: 'Edit',
-          icon: 'pi pi-fw pi-pencil',
-          items: [
-              {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-              {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-          ]
-      }
+          label: 'B part'
+      },
+      {
+        label: 'C part',
+        items: [
+            {label: 'sub part'},
+            {label: 'sub part'}
+        ]
+    },
+    {
+      label: 'C part',
+      items: [
+          {label: 'sub part'},
+          {label: 'sub part'}
+      ]
+  },
   ];
   }
 }
